38fab8a9adb7cb602a9f236db364ba8a88ba2a32
NattXCZ <natalka.joly@seznam.cz>
2024-07-28 23:13:11 +0200

    ...

--- src/methods/dtc_psychovisual.py
+++ src/methods/dtc_psychovisual.py
@@ -7,45 +7,11 @@ from skimage.metrics import structural_similarity as ssim
 from src.utils import video_processing_utils as vid_utils
 from src.utils import binary_utils as bnr
 from src.utils import string_utils
+from src.utils import motion_vectors_utils as mv
 
 ZIGZAG_INDICES = [
     (3, 1), (2, 2), (1, 3), (0, 4), (2, 3), (1, 4)
 ]
-
-########################
-def filter_motion_blocks(motion_blocks, remove_frame):
-    return [(frame, x, y) for frame, x, y in motion_blocks if frame not in remove_frame]
-########################
-
-def save_motion_blocks(motion_blocks, filename):
-    """Save motion blocks to a file."""
-    with open(filename, 'w') as f:
-        json.dump(motion_blocks, f)
-
-
-def load_motion_blocks(filename):
-    """Load motion blocks from a file."""
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-def calculate_ssim(img1, img2):
-    """Calculate the Structural Similarity Index (SSIM) between two images."""
-    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    ssim_value, _ = ssim(img1_gray, img2_gray, full=True)
-    return ssim_value
-
-
-def calculate_nc(img1, img2):
-    """Calculate the Normalized Correlation (NC) between two images."""
-    img1_norm = cv2.normalize(img1.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
-    img2_norm = cv2.normalize(img2.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
-    return np.mean(img1_norm * img2_norm)
-
-
-def determine_threshold(ssim_value, nc_value, alpha=0.5):
-    """Determine the threshold value based on SSIM and NC values. """
-    return alpha * ssim_value + (1 - alpha) * nc_value
 
 
 def zigzag_create_D(matrix):
@@ -108,43 +74,6 @@ def extracting_technique_abs(D):
             message_bits.append(0)
     return message_bits
 
-def detect_motion_blocks_and_T(properties):
-    """Detect motion blocks and calculate threshold values for each frame."""
-    motion_blocks = []
-    T_values = []
-    prev_frame = None
-
-    for frame in range(int(properties['frames'])):
-        if os.path.exists(f"frames/frame_{frame}.png"):
-            img_path = f"frames/frame_{frame}.png"
-        else:
-            continue
-
-        current_frame = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        if current_frame is None:
-            continue
-
-        if prev_frame is not None:
-            flow = cv2.calcOpticalFlowFarneback(prev_frame, current_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            magnitude, _ = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            
-            for i in range(0, current_frame.shape[0], 8):
-                for j in range(0, current_frame.shape[1], 8):
-                    block_magnitude = magnitude[i:i+8, j:j+8]
-                    avg_magnitude = np.mean(block_magnitude)
-                    if avg_magnitude >= 7:    
-                        motion_blocks.append((frame, j, i))
-            
-            ssim_value = ssim(prev_frame, current_frame)
-            nc_value = calculate_nc(prev_frame, current_frame)
-            T = determine_threshold(ssim_value, nc_value)
-            T_values.append(T)
-
-        prev_frame = current_frame
-
-    return motion_blocks, T_values
-
-
 def encode_dtc_psyschovisual(orig_video_path, message_path, motion_blocks_file, generate_threshold=True):
     """
     Encode a message into a video using the DTC Psychovisual LSB method.
@@ -161,9 +90,11 @@ def encode_dtc_psyschovisual(orig_video_path, message_path, motion_blocks_file, 
     message = bnr.string_to_binary_array(message_path)
     
     properties = vid_utils.video_to_rgb_frames(orig_video_path)
-    motion_blocks_w_I, T_values = detect_motion_blocks_and_T(properties)
+    motion_blocks_w_I, T_values = mv.detect_motion_blocks_and_T(properties)
     I_frame_indexes = vid_utils.get_I_frame_numbers(orig_video_path)
-    motion_blocks = filter_motion_blocks(motion_blocks_w_I, I_frame_indexes)
+    motion_blocks = mv.filter_motion_blocks(motion_blocks_w_I, I_frame_indexes)
+    
+    mv.save_motion_blocks(motion_blocks, motion_blocks_file)
     
     T_iterator = iter(T_values)
     message_index = 0
@@ -224,7 +155,7 @@ def decode_dtc_psyschovisual(stego_video_path, len_b_msg, motion_blocks_file, wr
     """
     properties = vid_utils.video_to_rgb_frames(stego_video_path)
     message = []
-    motion_blocks = load_motion_blocks(motion_blocks_file)
+    motion_blocks = mv.load_motion_blocks(motion_blocks_file)
 
     for frame, x, y in motion_blocks:
         if len(message) >= len_b_msg:
--- src/utils/motion_vectors_utils.py
+++ src/utils/motion_vectors_utils.py
@@ -1,110 +1,51 @@
-import numpy as np
-import cv2
 import os
-from src.utils import video_processing_utils as vid_utils
-from src.utils import binary_utils as bnr
-import glob
-######
+import json
+import cv2
+import numpy as np
 
-# Uklada video s vyraznenymi MV na snimcich 
-#funguje
-def count_frames_in_directory(frame_type):
-    file_path_pattern = f"tmp/{frame_type}_frames/frame_*.png"
-    files = glob.glob(file_path_pattern)
-    return len(files)
-
-def visualize_motion_vectors(properties, motion_vectors):
-    for frame in range(int(properties['frames'])):
-        # Najdi odpovídající snímek
-        if os.path.exists(f"tmp/P_frames/frame_{frame}.png"):
-            img_path = f"tmp/P_frames/frame_{frame}.png"
-        elif os.path.exists(f"tmp/B_frames/frame_{frame}.png"):
-            img_path = f"tmp/B_frames/frame_{frame}.png"
-        else:
-            continue
-
-        # Načti snímek v barevném formátu
-        img = cv2.imread(img_path)
-        if img is None:
-            continue
-
-        # Vykresli vektory pohybu pro tento snímek
-        for vector in motion_vectors:
-            if vector[0] == frame:  # pokud vektor patří k tomuto snímku
-                x, y = vector[1], vector[2]
-                cv2.rectangle(img, (x, y), (x+8, y+8), (0, 255, 0), 1)  # zelený rámeček
-                cv2.circle(img, (x+4, y+4), 2, (0, 0, 255), -1)  # červený bod uprostřed
-
-        # Zobraz snímek
-        cv2.imshow(f'Frame {frame}', img)
-        cv2.waitKey(30)  # Počkej 30 ms nebo dokud uživatel nestiskne klávesu
-
-    cv2.destroyAllWindows()
+from skimage.metrics import structural_similarity as ssim
 
 
+def filter_motion_blocks(motion_blocks, remove_frame):
+    """Filters a list of tuples (frame, x, y) and returns a new list that excludes tuples with frames present in remove_frame."""
+    return [(frame, x, y) for frame, x, y in motion_blocks if frame not in remove_frame]
 
+def save_motion_blocks(motion_blocks, filename):
+    """Save motion blocks to a file."""
+    with open(filename, 'w') as f:
+        json.dump(motion_blocks, f)
 
-def save_motion_vectors(properties, motion_vectors):
-    for frame in range(int(properties['frames'])):
-        # Najdi odpovídající snímek
-        if os.path.exists(f"tmp/P_frames/frame_{frame}.png"):
-            img_path = f"tmp/P_frames/frame_{frame}.png"
-        elif os.path.exists(f"tmp/B_frames/frame_{frame}.png"):
-            img_path = f"tmp/B_frames/frame_{frame}.png"
-        else:
-            continue
+def load_motion_blocks(filename):
+    """Load motion blocks from a file."""
+    with open(filename, 'r') as f:
+        return json.load(f)
 
-        # Načti snímek v barevném formátu
-        img = cv2.imread(img_path)
-        if img is None:
-            continue
+def calculate_ssim(img1, img2):
+    """Calculate the Structural Similarity Index (SSIM) between two images."""
+    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    ssim_value, _ = ssim(img1_gray, img2_gray, full=True)
+    return ssim_value
 
-        # Vykresli vektory pohybu pro tento snímek
-        for vector in motion_vectors:
-            if vector[0] == frame:  # pokud vektor patří k tomuto snímku
-                x, y = vector[1], vector[2]
-                cv2.rectangle(img, (x, y), (x+8, y+8), (0, 255, 0), 1)  # zelený rámeček
-                cv2.circle(img, (x+4, y+4), 2, (0, 0, 255), -1)  # červený bod uprostřed
+def calculate_nc(img1, img2):
+    """Calculate the Normalized Correlation (NC) between two images."""
+    img1_norm = cv2.normalize(img1.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    img2_norm = cv2.normalize(img2.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    return np.mean(img1_norm * img2_norm)
 
-        # Přepiš snímek na disku
-        cv2.imwrite(img_path, img)
-###################
+def determine_threshold(ssim_value, nc_value, alpha=0.5):
+    """Determine the threshold value based on SSIM and NC values. """
+    return alpha * ssim_value + (1 - alpha) * nc_value
 
-
-
-def save_video_w_motion_vector_blocks(orig_video_path, message_path, string_flag=False, flag_delete_dirs=True):
-    properties = vid_utils.video_to_IBP_frames(orig_video_path)
-
-    if string_flag:
-        message = bnr.string_to_binary_array(message_path)
-    else:
-        message = bnr.file_to_binary_1D_arr(message_path)
-    
-    motion_blocks = detect_motion_blocks(properties)
-    
-    #print(motion_blocks)
-    save_motion_vectors(properties, motion_blocks)
-    
-    
-    
-    vid_utils.reconstruct_video_from_IBP_frames(orig_video_path, properties)
-    
-    if flag_delete_dirs:
-        vid_utils.remove_dirs()
-        
-    print(message)
-
-
-
-def detect_motion_blocks(properties):
+def detect_motion_blocks_and_T(properties):
+    """Detect motion blocks and calculate threshold values for each frame."""
     motion_blocks = []
+    T_values = []
     prev_frame = None
 
     for frame in range(int(properties['frames'])):
-        if os.path.exists(f"tmp/P_frames/frame_{frame}.png"):
-            img_path = f"tmp/P_frames/frame_{frame}.png"
-        elif os.path.exists(f"tmp/B_frames/frame_{frame}.png"):
-            img_path = f"tmp/B_frames/frame_{frame}.png"
+        if os.path.exists(f"frames/frame_{frame}.png"):
+            img_path = f"frames/frame_{frame}.png"
         else:
             continue
 
@@ -113,43 +54,21 @@ def detect_motion_blocks(properties):
             continue
 
         if prev_frame is not None:
-            # Výpočet Dense Optical Flow
             flow = cv2.calcOpticalFlowFarneback(prev_frame, current_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            
-            # Výpočet magnitudy pohybu
             magnitude, _ = cv2.cartToPolar(flow[..., 0], flow[..., 1])
             
-            # Procházení bloků 8x8
             for i in range(0, current_frame.shape[0], 8):
                 for j in range(0, current_frame.shape[1], 8):
                     block_magnitude = magnitude[i:i+8, j:j+8]
-                    
-                    # Výpočet průměrné magnitudy bloku
                     avg_magnitude = np.mean(block_magnitude)
-                    
-                    # Detekce pohybu, pokud průměrná magnituda je >= 7
-                    #FIXME: puvodne 7
                     if avg_magnitude >= 7:    
                         motion_blocks.append((frame, j, i))
+            
+            ssim_value = ssim(prev_frame, current_frame)
+            nc_value = calculate_nc(prev_frame, current_frame)
+            T = determine_threshold(ssim_value, nc_value)
+            T_values.append(T)
 
         prev_frame = current_frame
 
-    return motion_blocks
-
-
-
-def write_message_to_file(message, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(message)
-
-def main():
-    video_file_path = r"data_testing\input_video.mp4"
-    message = "Hello how are you. I am fine what about you"
-    
-    save_video_w_motion_vector_blocks(video_file_path, message, string_flag=True)
-    
-    stego_video_path = r"video.avi"
-    output_path = r"./decoded_message.txt"
-
-if __name__ == "__main__":
-    main()
+    return motion_blocks, T_values
