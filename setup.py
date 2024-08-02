d17a8d42d9735a679d3ef96e845c7ac96be4904c
NattXCZ <natalka.joly@seznam.cz>
2024-08-03 01:27:05 +0200

    .

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='Video steganography implementation of three methods',
     version='0.1',
-    author='Natalie Trhlikova', 
-    author_email='xxxxxxx@mail.com',
+    author='Natalie Trhlikova',
+    author_email='natalie.trhlikova01@upol.cz',
     packages=find_packages(),
-    long_description=open('README.md').read()
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'pywavelets',
+        'galois',
+        'scikit-image',
+        'tkinter',
+    ],
+    python_requires='>=3.9',
 )
